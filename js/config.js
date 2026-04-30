0f7bac0138c005775b9b5177f5b36794ee951137
hostwellstudios <hostwell.studios@gmail.com>
2026-04-30 14:18:05 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -10,8 +10,8 @@
 // party app. For production apps, move admin writes to an Edge Function.
 
 const CONFIG = {
-  supabaseUrl:        'YOUR_SUPABASE_URL',
-  supabaseAnonKey:    'YOUR_SUPABASE_ANON_KEY',
-  supabaseServiceKey: 'YOUR_SUPABASE_SERVICE_ROLE_KEY',
-  adminPassword:      'spritz2025'
+  supabaseUrl:        '<https://nvuzpyrufeqblqozyeln.supabase.co>',
+  supabaseAnonKey:    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52dXpweXJ1ZmVxYmxxb3p5ZWxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1NjgxNjAsImV4cCI6MjA5MzE0NDE2MH0.8wy12nxlex0bKcb4atFQer5C4rc_paej8rZ784dM8Ew',
+  supabaseServiceKey: 'sb_secret_6jEGT80z-9zMfDChemqN2g_PRMXBkmT',
+  adminPassword:      'spritz2026'
 };
